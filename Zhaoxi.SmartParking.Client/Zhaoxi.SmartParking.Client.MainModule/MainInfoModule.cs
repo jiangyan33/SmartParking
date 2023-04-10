5750b81b2d89e30365ed5a26046640e3736cfaac
shijiangshan <shijiangshan@bjknrt.com>
2023-04-10 18:12:31 +0800

    主页未完成

---
+++ Zhaoxi.SmartParking.Client/Zhaoxi.SmartParking.Client.MainModule/MainInfoModule.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zhaoxi.SmartParking.Client.MainModule.Views;
+
+namespace Zhaoxi.SmartParking.Client.MainModule
+{
+    public class MainInfoModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("MainViewRegion", "DashboardView");
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<DashboardView>();
+        }
+    }
+}
---
+++ Zhaoxi.SmartParking.Client/Zhaoxi.SmartParking.Client.MainModule/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhaoxi.SmartParking.Client.MainModule.ViewModels
+{
+    public class DashboardViewModel
+    {
+    }
+}
